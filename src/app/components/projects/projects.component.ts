ee7f509375f31602f819b9dc484272b4b338af5f
Benjamin Looi <benjaminlooidt@gmail.com>
2020-01-22 01:04:05 +0800

    projects page from json data instead of hard coded

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {projectsData} from './projects_data.js';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
+
 export class ProjectsComponent implements OnInit {
+
+  projectData: any;
+  numbers: number[];
 
   constructor() { }
 
   ngOnInit() {
+    this.projectData = projectsData;
+
+    //https://stackoverflow.com/questions/34405878/way-to-ngfor-loop-defined-number-of-times-instead-of-repeating-over-array
+    this.numbers = Array(Math.ceil(this.projectData.length/3)).fill(0).map((x,i)=>i)
   }
 
 }
---
+++ src/app/components/projects/projects_data.js
@@ -0,0 +1,79 @@
+export const projectsData = [
+  {
+    imageSrc: 'assets/ledo.png',
+    imageAlt: 'image cap',
+    projectLink: 'https://theledo.netlify.com/',
+    projectTitle: 'Ledo',
+    projectDesc: 'Todo app'
+  },
+  {
+    imageSrc: 'assets/seo_quiz_banner.jpg',
+    imageAlt: 'SEO Quiz image cap',
+    projectLink: 'https://seopressor.com/seo-quiz',
+    projectTitle: 'SEO Quiz',
+    projectDesc: 'Jquery'
+  },
+  {
+    imageSrc: 'assets/nyheadline_cropped.jpg',
+    imageAlt: 'nyheadline image cap',
+    projectLink: 'https://nyheadline.com',
+    projectTitle: 'NYheadline',
+    projectDesc: 'Built with Angular + Firebase'
+  },
+  {
+    imageSrc: 'assets/utn.jpg',
+    imageAlt: 'Threadify ng image cap',
+    projectLink: 'https://utn.netlify.com',
+    projectTitle: 'Utn',
+    projectDesc: 'Frontend: Angular<br>Backend: Half with PHP, and another half with Nodejs Express'
+  },
+  {
+    imageSrc: null,
+    imageAlt: 'Threadify ng image cap',
+    projectLink: 'https://benjaminlooi.github.io/threadify-ng',
+    projectTitle: 'Threadify-ng',
+    projectDesc: 'Built with Angular + Firebase'
+  },
+  {
+    imageSrc: null,
+    imageAlt: 'Facemash image cap',
+    projectLink: 'https://facemash-vue.herokuapp.com/',
+    projectTitle: 'Facemash',
+    projectDesc: 'Democracy at it\'s peak.<br>Vote for the hottest lecturer!<br>Made with vue + vuetify'
+  },
+  {
+    imageSrc: 'assets/prs.jpg',
+    imageAlt: 'Pet Registration System image cap',
+    projectLink: 'http://bensanity.epizy.com/prs',
+    projectTitle: 'Pet Registration System',
+    projectDesc: 'Web Prog. Class Assignment<br>PHP + MySQL'
+  },
+  {
+    imageSrc: null,
+    imageAlt: 'threadify image cap',
+    projectLink: 'http://bensanity.epizy.com/threadify',
+    projectTitle: 'Threadify',
+    projectDesc: 'Built with PHP and mySQL'
+  },
+  {
+    imageSrc: 'assets/r-jerome.jpg',
+    imageAlt: 'jerome image cap',
+    projectLink: 'https://r-jerome.netlify.com/',
+    projectTitle: 'Rayymond Jerome\'s Portfolio',
+    projectDesc: 'Front end: vuejs<br>firebase'
+  },
+  {
+    imageSrc: 'assets/story-of-ray.jpg',
+    imageAlt: 'The story of ray image cap',
+    projectLink: 'https://benjaminlooi.github.io/the-journey-of-ray-ori/',
+    projectTitle: 'The story of ray',
+    projectDesc: 'System Administration Class Assignment<br>HTML'
+  },
+  // {
+  //   imageSrc: 'vvvvvvvvvvv',
+  //   imageAlt: 'vvvvvvvvvvv',
+  //   projectLink: 'vvvvvvvvvvv',
+  //   projectTitle: 'vvvvvvvvvvv',
+  //   projectDesc: 'vvvvvvvvvvv'
+  // },
+]
