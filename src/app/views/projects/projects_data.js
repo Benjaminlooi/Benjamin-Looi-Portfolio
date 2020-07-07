cf6a560d2dcc0b97cf6205a2bf1f62d72199d94e
Benjaminlooi <benjaminlooidt@gmail.com>
2020-07-07 16:39:28 +0800

    Add covid19my to projects

--- src/app/views/projects/projects_data.js
+++ src/app/views/projects/projects_data.js
@@ -1,12 +1,5 @@
 export const projectsData = [
   {
-    imageSrc: 'assets/ledo.png',
-    imageAlt: 'image cap',
-    projectLink: 'https://theledo.netlify.com/',
-    projectTitle: 'Ledo',
-    projectDesc: 'Todo app'
-  },
-  {
     imageSrc: 'assets/seo_quiz_banner.jpg',
     imageAlt: 'SEO Quiz image cap',
     projectLink: 'https://seopressor.com/seo-quiz',
@@ -21,26 +14,47 @@ export const projectsData = [
     projectDesc: 'Built with Angular w/ MDBootstrap & Firebase'
   },
   {
-    imageSrc: 'assets/utn.jpg',
-    imageAlt: 'Threadify ng image cap',
-    projectLink: 'https://utn.netlify.com',
-    projectTitle: 'Utn',
-    projectDesc: 'Frontend: Angular<br>Backend: Half with PHP, and another half with Nodejs + Express'
+    imageSrc: 'assets/covid19my.png',
+    imageAlt: 'Covid19My image cap',
+    projectLink: 'https://covid19my.netlify.app/',
+    projectTitle: 'Covid19 My',
+    projectDesc: 'Find out the lastest updates and essential information about the current outbreak in Malaysia, COVID-19 | News & Stats for coronavirus in Malaysia<br>Frontend: VueJS<br>Database: Firebase firestore<br>Backend: ExpressJS w/ puppeteer'
   },
   {
-    imageSrc: null,
-    imageAlt: 'Threadify ng image cap',
-    projectLink: 'https://benjaminlooi.github.io/threadify-ng',
-    projectTitle: 'Threadify-ng',
-    projectDesc: 'Built with Angular + Firebase'
+    imageSrc: 'assets/r-jerome.jpg',
+    imageAlt: 'jerome image cap',
+    projectLink: 'https://rayymondjerome.netlify.com/',
+    projectTitle: 'Rayymond Jerome\'s Portfolio',
+    projectDesc: 'Front end: vuejs<br>firebase'
   },
   {
-    imageSrc: null,
-    imageAlt: 'Facemash image cap',
-    projectLink: 'https://facemash-vue.herokuapp.com/',
-    projectTitle: 'Facemash',
-    projectDesc: 'Democracy at it\'s peak.<br>Vote for the hottest lecturer!<br>Made with vue + vuetify'
+    imageSrc: 'assets/ledo.png',
+    imageAlt: 'image cap',
+    projectLink: 'https://theledo.netlify.com/',
+    projectTitle: 'Ledo',
+    projectDesc: 'Todo app'
   },
+  // {
+  //   imageSrc: 'assets/utn.jpg',
+  //   imageAlt: 'Threadify ng image cap',
+  //   projectLink: 'https://utn.netlify.com',
+  //   projectTitle: 'Utn',
+  //   projectDesc: 'Frontend: Angular<br>Backend: Half with PHP, and another half with Nodejs + Express'
+  // },
+  // {
+  //   imageSrc: null,
+  //   imageAlt: 'Threadify ng image cap',
+  //   projectLink: 'https://benjaminlooi.github.io/threadify-ng',
+  //   projectTitle: 'Threadify-ng',
+  //   projectDesc: 'Built with Angular + Firebase'
+  // },
+  // {
+  //   imageSrc: null,
+  //   imageAlt: 'Facemash image cap',
+  //   projectLink: 'https://facemash-vue.herokuapp.com/',
+  //   projectTitle: 'Facemash',
+  //   projectDesc: 'Democracy at it\'s peak.<br>Vote for the hottest lecturer!<br>Made with vue + vuetify'
+  // },
   {
     imageSrc: 'assets/prs.jpg',
     imageAlt: 'Pet Registration System image cap',
@@ -48,20 +62,13 @@ export const projectsData = [
     projectTitle: 'Pet Registration System',
     projectDesc: 'Web Prog. Class Assignment<br>HTML+CSS+PHP+MySQL w/ Bootstrap'
   },
-  {
-    imageSrc: null,
-    imageAlt: 'threadify image cap',
-    projectLink: 'http://bensanity.epizy.com/threadify',
-    projectTitle: 'Threadify',
-    projectDesc: 'Built with PHP and mySQL'
-  },
-  {
-    imageSrc: 'assets/r-jerome.jpg',
-    imageAlt: 'jerome image cap',
-    projectLink: 'https://r-jerome.netlify.com/',
-    projectTitle: 'Rayymond Jerome\'s Portfolio',
-    projectDesc: 'Front end: vuejs<br>firebase'
-  },
+  // {
+  //   imageSrc: null,
+  //   imageAlt: 'threadify image cap',
+  //   projectLink: 'http://bensanity.epizy.com/threadify',
+  //   projectTitle: 'Threadify',
+  //   projectDesc: 'Built with PHP and mySQL'
+  // },
   {
     imageSrc: 'assets/story-of-ray.jpg',
     imageAlt: 'The story of ray image cap',
@@ -69,20 +76,20 @@ export const projectsData = [
     projectTitle: 'The story of ray',
     projectDesc: 'System Administration Class Assignment<br>HTML+CSS w/ Bootstrap'
   },
-  {
-    imageSrc: null,
-    imageAlt: null,
-    projectLink: 'https://steamedbum.netlify.com/',
-    projectTitle: 'SteamedBum',
-    projectDesc: 'my VueJs playground'
-  },
-  {
-    imageSrc: null,
-    imageAlt: null,
-    projectLink: 'https://steamedbanana.netlify.com/',
-    projectTitle: 'SteamedBanana',
-    projectDesc: 'my Angular playground'
-  },
+  // {
+  //   imageSrc: null,
+  //   imageAlt: null,
+  //   projectLink: 'https://steamedbum.netlify.com/',
+  //   projectTitle: 'SteamedBum',
+  //   projectDesc: 'my VueJs playground'
+  // },
+  // {
+  //   imageSrc: null,
+  //   imageAlt: null,
+  //   projectLink: 'https://steamedbanana.netlify.com/',
+  //   projectTitle: 'SteamedBanana',
+  //   projectDesc: 'my Angular playground'
+  // },
   // {
   //   imageSrc: 'vvvvvvvvvvv',
   //   imageAlt: 'vvvvvvvvvvv',
